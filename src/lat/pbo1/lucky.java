57e77f67af2ac7b1efc0dc99b04bf26e388d5602
Lucky fitriady erwandy saputra <luckyfitriady@gmail.com>
2024-07-04 19:42:22 +0800

    initial commit

---
+++ src/lat/pbo1/lucky.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lat.pbo1;
+
+/**
+ *
+ * @author lucky
+ */
+public class lucky {
+    String nama = "Lucky";
+    String kampus = "Uniska";
+    int npm = 2010010051;
+
+public class static lucky (String[]args);{
+    System.out.println("Nama: " + getnama);
+    System.out.println("kampus: " + getkampus);
+    System.out.println("npm: " + getnpm);
+
+    setnama("Lucky");
+    setkampus("uniska");
+    setnpm(2010010051");
+
+    
+    System.out.println("nama: " +getNama);
+    System.out.println("kampus: " +getkampus);
+    System.out.println("npm " +getnpm;
+    }
+}
+
+    
+    
+    
